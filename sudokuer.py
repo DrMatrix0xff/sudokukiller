2a4fa4f2e4afdf25455cbd586252ef3333feb9e3
Whoo Lishin <lixin.hu@ebuinfo.com>
2016-04-19 11:35:44 +0800

    less loop opt

--- sudokuer.py
+++ sudokuer.py
@@ -5,6 +5,16 @@ from collections import namedtuple
 
 Entry = namedtuple('Entry', 'pos val')
 
+ROWS = [range(9*i, 9*(i+1)) for i in range(9)]
+COLUMNS = [range(i, i+80, 9) for i in range(9)]
+BOXES = []
+for i in range(9):
+    b = []
+    for l in range((i/3)*3, (i/3)*3+3):
+        for c in range((i%3)*3, (i%3)*3+3):
+            b.append(9*l+c)
+    BOXES.append(b)
+
 def make_sudoku_solver(puzzle):
     solver = SudokuSolver()
     solver.set_sudoku(puzzle)
@@ -23,7 +33,7 @@ class SudokuSolver(object):
 
     def check_sudoku(self):
         for pos in range(81):
-            #print pos,
+            print pos,
             if self.puzzle[pos] == 0:
                 continue
             else:
@@ -36,32 +46,51 @@ class SudokuSolver(object):
         line = pos / 9
         col = pos % 9
         box = (line / 3) * 3 + col / 3
-        i = line * 9
-        while i < (line+1) * 9:
+        row = ROWS[line]
+        column = COLUMNS[col]
+        boxbox = BOXES[box]
+        for i in row:
             if initial_check and i == pos:
-                i += 1
                 continue
-            if self.puzzle[i] != 0 and self.puzzle[i] == x:
+            if self.puzzle[i] == x:
                 return False
-            i += 1
-        i = col
-        while i <= (col + 72):
+        for i in column:
             if initial_check and i == pos:
-                i += 9
                 continue
-            if self.puzzle[i] != 0 and self.puzzle[i] == x:
+            if self.puzzle[i] == x:
                 return False
-            i += 9
-        topleft = (box / 3) * 27 + (box % 3) * 3
-        i = topleft
-        while i <= (topleft+20):
-            for j in range(3):
-                if initial_check and (i+j) == pos:
-                    continue
-                if self.puzzle[i+j] != 0 and self.puzzle[i+j] == x:
-                    return False
-            i += 9
+        for i in boxbox:
+            if initial_check and i == pos:
+                continue
+            if self.puzzle[i] == x:
+                return False
         return True
+        #i = line * 9
+        #while i < (line+1) * 9:
+        #    if initial_check and i == pos:
+        #        i += 1
+        #        continue
+        #    if self.puzzle[i] != 0 and self.puzzle[i] == x:
+        #        return False
+        #    i += 1
+        #i = col
+        #while i <= (col + 72):
+        #    if initial_check and i == pos:
+        #        i += 9
+        #        continue
+        #    if self.puzzle[i] != 0 and self.puzzle[i] == x:
+        #        return False
+        #    i += 9
+        #topleft = (box / 3) * 27 + (box % 3) * 3
+        #i = topleft
+        #while i <= (topleft+20):
+        #    for j in range(3):
+        #        if initial_check and (i+j) == pos:
+        #            continue
+        #        if self.puzzle[i+j] != 0 and self.puzzle[i+j] == x:
+        #            return False
+        #    i += 9
+        #return True
 
     def make_guess(self, pos, b):
         guess = b+1
@@ -128,15 +157,25 @@ if __name__ == '__main__':
         #0,0,0, 0,2,0, 3,0,0,
         #0,5,0, 4,0,0, 7,0,0,
         #===================
-        9,3,0, 0,0,0, 4,8,0,
-        0,0,4, 0,0,0, 0,0,3,
-        6,0,1, 0,3,0, 0,9,5,
-        1,0,0, 3,0,8, 0,5,0,
-        0,0,0, 1,9,6, 0,0,0,
-        0,0,0, 7,2,5, 0,0,0,
-        0,0,2, 0,0,0, 0,0,9,
-        8,4,0, 0,0,0, 5,1,0,
-        5,0,9, 0,8,0, 0,4,7,
+        #9,3,0, 0,0,0, 4,8,0,
+        #0,0,4, 0,0,0, 0,0,3,
+        #6,0,1, 0,3,0, 0,9,5,
+        #1,0,0, 3,0,8, 0,5,0,
+        #0,0,0, 1,9,6, 0,0,0,
+        #0,0,0, 7,2,5, 0,0,0,
+        #0,0,2, 0,0,0, 0,0,9,
+        #8,4,0, 0,0,0, 5,1,0,
+        #5,0,9, 0,8,0, 0,4,7,
+        #===================
+        0,0,0, 0,0,6, 0,0,0,
+        0,5,9, 0,0,0, 0,0,8,
+        2,0,0, 0,0,8, 0,0,0,
+        0,4,5, 0,0,0, 0,0,0,
+        0,0,3, 0,0,0, 0,0,0,
+        0,0,6, 0,0,3, 0,5,4,
+        0,0,0, 3,2,5, 0,0,6,
+        0,0,0, 0,0,0, 0,0,0,
+        0,0,0, 0,0,0, 0,0,0,
     ])
     sdksolver.solve()
     sdksolver.show_solution()
